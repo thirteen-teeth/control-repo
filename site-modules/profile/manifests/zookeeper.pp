40344e35d09ca9d6fea99e56c3a4442ccda02802
Eric Morris <emorris@athenahealth.com>
2021-05-28 18:44:02 -0400

    systemd changes

--- site-modules/profile/manifests/zookeeper.pp
+++ site-modules/profile/manifests/zookeeper.pp
@@ -5,8 +5,10 @@ class profile::zookeeper {
   require profile::java
 
   class { 'zookeeper':
-    install_method  => 'archive',
-    archive_version => '3.6.3',
+    install_method      => 'archive',
+    archive_version     => '3.6.3',
+    service_provider    => 'systemd',
+    manage_service_file => true,
   }
 
 }
