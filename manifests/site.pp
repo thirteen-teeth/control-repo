fe52b079da91b713d57992bc3bf7df0930fe2117
Eric Morris <emorris@athenahealth.com>
2021-07-13 09:50:19 -0400

    oh

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,18 @@
 node default {
-  hiera_include('classes')
+  $pp_role = $trusted['extensions']['pp_role']
+  $role = "role::${pp_role}"
+
+  case $pp_role {
+    default: {
+      if (defined(Type[Class[$role]])) {
+        include $role
+      }
+      else {
+        fail("No matching class ${role} found")
+      }
+    }
+    undef, '': {
+      fail("${trusted['certname']} does not have a pp_role trusted fact!")
+    }
+  }
 }
