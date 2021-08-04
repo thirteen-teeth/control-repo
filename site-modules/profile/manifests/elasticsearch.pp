b7ddfe388bbba2bec12f0039f102c7a1d0c2d67e
Eric Morris <emorris@athenahealth.com>
2021-08-04 02:15:53 -0400

    changes

--- site-modules/profile/manifests/elasticsearch.pp
+++ site-modules/profile/manifests/elasticsearch.pp
@@ -1,8 +1,27 @@
 # comment
-class profile::elasticsearch {
+class profile::elasticsearch (
+  String $ssl_dir = '/etc/puppetlabs/puppet/ssl',
+  String $user = 'elasticsearch',
+) {
+
   include elasticsearch
   include elastic_stack::repo
   include prometheus::elasticsearch_exporter
   include profile::java
   include profile::java_ks
+
+  posix_acl { $ssl_dir:
+    action     => set,
+    permission => ["user:${user}:r-X"],
+    provider   => 'posixacl',
+    recursive  => true,
+  }
+
+  posix_acl { "${ssl_dir}/private_keys/${facts['fqdn']}.pem":
+    action     => set,
+    permission => ["user:${user}:r"],
+    provider   => 'posixacl',
+  }
+
+
 }
