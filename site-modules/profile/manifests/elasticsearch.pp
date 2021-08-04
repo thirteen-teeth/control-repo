26f412644ba88030f384e42ead9f9ffd2b68ca04
Eric Morris <emorris@athenahealth.com>
2021-08-04 01:47:52 -0400

    changes

--- site-modules/profile/manifests/elasticsearch.pp
+++ site-modules/profile/manifests/elasticsearch.pp
@@ -3,4 +3,6 @@ class profile::elasticsearch {
   include elasticsearch
   include elastic_stack::repo
   include prometheus::elasticsearch_exporter
+  include profile::java
+  include profile::java_ks
 }
