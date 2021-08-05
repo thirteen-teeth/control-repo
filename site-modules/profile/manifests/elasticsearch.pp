358212fc2ebaad2d72b933c5e60dc1802c661adf
Eric Morris <emorris@athenahealth.com>
2021-08-05 18:46:04 -0400

    changes

--- site-modules/profile/manifests/elasticsearch.pp
+++ site-modules/profile/manifests/elasticsearch.pp
@@ -7,24 +7,4 @@ class profile::elasticsearch (
   include elasticsearch
   include elastic_stack::repo
   include prometheus::elasticsearch_exporter
-  include profile::java
-
-  posix_acl { $ssl_dir:
-    action     => set,
-    permission => ["user:${user}:r-X"],
-    provider   => 'posixacl',
-    recursive  => true,
-  }
-
-  posix_acl { "${ssl_dir}/private_keys/${facts['fqdn']}.pem":
-    action     => set,
-    permission => ["user:${user}:r"],
-    provider   => 'posixacl',
-  }
-
-  file_line { 'ES_JAVA_HOME':
-    path => '/etc/sysconfig/elasticsearch',
-    line => 'ES_JAVA_HOME=/usr/lib/jvm/jre/bin',
-  }
-
 }
