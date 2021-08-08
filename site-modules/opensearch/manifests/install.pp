955ce2e548ab6178247b49af5e83156c15413131
Eric Morris <emorris@athenahealth.com>
2021-08-08 14:34:53 -0400

    changes

--- site-modules/opensearch/manifests/install.pp
+++ site-modules/opensearch/manifests/install.pp
@@ -3,7 +3,7 @@ class opensearch::install {
 
   archive { "${opensearch::download_target_dir}/${opensearch::artifact_name}":
     ensure       => $opensearch::ensure,
-    extract      => $opensearch::ensure,
+    extract      => true,
     extract_path => $opensearch::extract_dir,
     source       => $opensearch::artifact_full_url,
     creates      => $opensearch::install_dir,
