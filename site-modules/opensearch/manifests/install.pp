3af936a4a142499934d26c1ea253fcfa2c6bf894
Eric Morris <emorris@athenahealth.com>
2021-08-09 00:34:34 -0400

    changes

--- site-modules/opensearch/manifests/install.pp
+++ site-modules/opensearch/manifests/install.pp
@@ -2,11 +2,12 @@
 class opensearch::install {
 
   $fix_permissions = 'install dir permissions fix'
+  $copy_certs = 'copy puppet certs'
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
   user { $opensearch::username:
     ensure => $opensearch::ensure,
     shell  => '/sbin/nologin',
-    groups => ['puppet'],
   }
 
   File {
@@ -22,7 +23,6 @@ class opensearch::install {
     mode   => '0750',
   }
 
-  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
   archive { "${opensearch::download_target_dir}/${opensearch::artifact_name}":
     ensure       => $opensearch::ensure,
     extract      => true,
@@ -30,7 +30,12 @@ class opensearch::install {
     source       => $opensearch::artifact_full_url,
     creates      => $opensearch::install_dir,
     require      => User[$opensearch::username],
-    notify       => Exec[$fix_permissions],
+    notify       => Exec[$copy_certs],
+  }
+  exec { $copy_certs:
+    command     => "cp -r /etc/puppetlabs/puppet/ssl ${opensearch::install_dir}",
+    refreshonly => true,
+    notify      => Exec[$fix_permissions],
   }
   exec { $fix_permissions:
     command     => "chown -R ${opensearch::username}:${opensearch::username} ${opensearch::install_dir}",
