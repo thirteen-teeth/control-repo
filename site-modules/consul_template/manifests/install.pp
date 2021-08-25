d324cd139e983911b2ee6c55d19546f5b4905427
thirteen-teeth <emorris2@outlook.com>
2021-08-25 16:30:11 -0400

    changes

--- site-modules/consul_template/manifests/install.pp
+++ site-modules/consul_template/manifests/install.pp
@@ -26,10 +26,10 @@ class consul_template::install {
     mode   => '0750',
   }
 
-  archive { $consul_template::artifact_name:
+  archive { "${consul_template::download_target_dir}/${consul_template::artifact_name}":
     ensure       => $consul_template::ensure,
-    path         => "${consul_template::download_target_dir}/${consul_template::artifact_name}",
-    source       => $consul_template::artifact_full_url,
+#    source       => $consul_template::artifact_full_url,
+    source       => 'https://releases.hashicorp.com/consul-template/0.27.0/consul-template_0.27.0_linux_amd64.zip'
     extract      => true,
     extract_path => $consul_template::install_dir,
     creates      => "${consul_template::install_dir}/${consul_template::bin}",
