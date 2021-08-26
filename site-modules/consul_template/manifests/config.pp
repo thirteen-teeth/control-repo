f5901a2fca397346e04ccd57721cd36381912098
thirteen-teeth <emorris2@outlook.com>
2021-08-26 00:15:53 -0400

    changes

--- site-modules/consul_template/manifests/config.pp
+++ site-modules/consul_template/manifests/config.pp
@@ -4,17 +4,18 @@ class consul_template::config {
   $config_file = 'config.hcl'
   $config_dir = $consul_template::config_dir
 
+  File {
+    owner => $consul_template::username,
+    group => $consul_template::username,
+  }
+
   file { $config_dir:
     ensure => directory,
     mode   => '0750',
-    owner  => $consul_template::username,
-    group  => $consul_template::username,
   }
 
   file { "${config_dir}/${config_file}":
     mode    => '0640',
-    owner   => $consul_template::username,
-    group   => $consul_template::username,
     content => template("consul_template/${config_file}.erb"),
     require => File[$config_dir],
   }
