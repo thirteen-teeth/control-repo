38e2b43b94259b56cf933ce8747ff0512ceac608
thirteen-teeth <emorris2@outlook.com>
2021-08-26 00:10:19 -0400

    changes

--- site-modules/consul_template/manifests/config.pp
+++ site-modules/consul_template/manifests/config.pp
@@ -5,7 +5,6 @@ class consul_template::config {
   $config_dir = $consul_template::config_dir
 
   file { "${config_dir}/${config_file}":
-    ensure  => $consul_template::ensure,
     mode    => '0750',
     owner   => $consul_template::username,
     group   => $consul_template::username,
