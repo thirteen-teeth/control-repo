44f30184a27b7f23f62ace06608ecd55c4fd4dec
thirteen-teeth <emorris2@outlook.com>
2021-10-15 22:50:35 -0400

    dr spock

--- site-modules/profile/manifests/opensearch.pp
+++ site-modules/profile/manifests/opensearch.pp
@@ -3,7 +3,7 @@ class profile::opensearch () {
 
   include opensearch
 
-  $cert_files = [ 'cert', 'private', 'ca']
+  $cert_files = [ 'cert', 'private', 'ca', 'dr_spock']
   $cert_files.each |$name| {
     file { "/tmp/${name}.tpl":
       content => template("profile/consul_template/${name}.tpl.erb")
