b8e4fb2f24a8f165c890aab1228e12d914b51a05
thirteen-teeth <emorris2@outlook.com>
2021-10-18 00:42:52 -0400

    riker william t

--- site-modules/profile/manifests/opensearch.pp
+++ site-modules/profile/manifests/opensearch.pp
@@ -3,7 +3,7 @@ class profile::opensearch () {
 
   include opensearch
 
-  $cert_files = [ 'cert', 'private', 'ca', 'spock', 'spock_key']
+  $cert_files = [ 'cert', 'private', 'ca', 'riker', 'riker_private']
   $cert_files.each |$name| {
     file { "/tmp/${name}.tpl":
       content => template("profile/consul_template/${name}.tpl.erb")
