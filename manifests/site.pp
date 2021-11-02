f73216b3d28dbb5e361834d2388ce2a0fa2f55e4
thirteen-teeth <emorris2@outlook.com>
2021-11-02 17:57:53 -0400

    changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,10 @@
 node default {
   $pp_role = $trusted['extensions']['pp_role']
   $role = "role::${pp_role}"
-  include $role
+
+  if $role {
+    include $role
+  } else {
+    fail('No pp_role is set.')
+  }
 }
