a6775165ede0f9398c4c4c0306d4341bcc3de702
thirteen-teeth <emorris2@outlook.com>
2021-11-19 21:51:48 -0500

    changes!

--- site-modules/opensearch/manifests/plugin.pp
+++ site-modules/opensearch/manifests/plugin.pp
@@ -2,5 +2,9 @@
 define opensearch::plugin (
   Stdlib::HTTPUrl $url,
 ) {
-
+  $exe = "${opensearch::install_dir}/bin/opensearch-plugin"
+  exec { "${title} plugin":
+    command => "${exe} install -b ${url}",
+    unless  => "${exe} list | grep ${title}",
+  }
 }
